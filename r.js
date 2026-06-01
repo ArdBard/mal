2c0e61b5486ea88fd01a0e6e99da27305894e732
ArdBard <151861131+ArdBard@users.noreply.github.com>
2026-06-01 10:26:05 +0200

    Update r.js

--- r.js
+++ r.js
@@ -3,16 +3,16 @@
   const html = await res.text();
 
   const found =
-    /Jméno:<\/label>\s*<div class="col-sm-9">\s*<p class="form-control-static">\s*Bc\. Jan Kupsa\s*<\/p>/s.test(
+    /Jméno:<\/label>\s*<div class="col-sm-9">\s*<p class="form-control-static">\s*Ing\. Radim Kostelník\s*<\/p>/s.test(
       html
     );
 
   if (found) {
     const body = new URLSearchParams({
-      sso: 212628051,
+      sso: 212627109,
       desc: "oblast Data Security: \nposuzování bezpečnosti AI nástrojů a jejich využití\nICT Risk Analýza aplikací\nschvalování ticektů/žádostí:\nProcesy v oblasti databází a dat, CAR změny, COPR + CyberSec Vendor Checks, DTR žádosti, UAM - matice rolí\nReverzní Bodybuilder",
-      table: "6C6/6",
-      mobile: "+420720234843"
+      table: "6C6/8",
+      mobile: "++420601073504"
     });
 
     try {
